51056f45c9dfe4f6ff671d2de42a1ebb83d3184d
jiancheng <wangjc25@lenovo.com>
2021-07-22 14:04:15 +0800

    starter0.0.1

---
+++ starter-lock/src/main/java/org/jcm/lock/starterlock/LockProperties.java
@@ -0,0 +1,12 @@
+package org.jcm.lock.starterlock;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * lock properties
+ *
+ * @author jcwang
+ */
+@ConfigurationProperties(prefix = "jcm.lock")
+public class LockProperties {
+}
